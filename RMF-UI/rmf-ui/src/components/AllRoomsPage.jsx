8df8c6390db84fe20224b9cfd7f51adbefb97957
Rahuldutta387 <rahuldutta387@gmail.com>
2024-07-23 00:55:42 +0530

    loader changes

--- RMF-UI/rmf-ui/src/components/AllRoomsPage.jsx
+++ RMF-UI/rmf-ui/src/components/AllRoomsPage.jsx
@@ -3,6 +3,7 @@ import NavBarRMF from "./shared/NavBarRMF";
 import "./AllRoomPage.css";
 import axios from "axios";
 import Button from "@mui/material/Button";
+import Loader from "../components/shared/Loader.jsx";
 
 const AllRoomsPage = () => {
   var tryVal = 0;
@@ -10,7 +11,9 @@ const AllRoomsPage = () => {
   const [skip, setSkip] = useState(0);
   const [isNext, setIsNext] = useState(false);
   const [isPrev, setIsPrev] = useState(true);
+  const [isLoader, setIsLoader] = useState(false);
   function getRoomList() {
+    setIsLoader(true);
     axios
       .get("/api/RoomMateFinder/getRoomDetails?skip=" + skip + "&limit=10")
       .then((res) => {
@@ -20,11 +23,13 @@ const AllRoomsPage = () => {
         if (skip <= 0) {
           setIsPrev(true);
         }
-
+        setIsLoader(false);
         setIsNext(!res.data.isNext);
         setRoomList(res.data.roomDetails);
       })
-      .catch((err) => {});
+      .catch((err) => {
+        setIsLoader(false);
+      });
   }
   useEffect(() => {
     getRoomList();
@@ -35,47 +40,62 @@ const AllRoomsPage = () => {
   const handlePrevButton = () => {
     setSkip((skip) => skip - 10);
   };
+  let loader = (
+    <>
+      <Loader></Loader>
+    </>
+  );
   return (
     <>
       <NavBarRMF></NavBarRMF>
-      <div className="mainContainer">
-        {roomList.map((res) => (
-          <div className="user-profile-card">
-            <div className="avatar-section">
-              {/* Avatar image would go here */}
-              <div className="avatar-placeholder"></div>
-            </div>
-            <div className="user-info">
-              <h2>{res.name}</h2>
-              <p>
-                {res.address.area}, {res.address.city}, {res.address.state}
-              </p>
-            </div>
-            <div className="user-details">
-              <div className="detail rent">Rent ₹ {res.amount}</div>
-              <div className="detail looking-for">
-                Looking for {res.requirement.gender}
+      {isLoader ? (
+        loader
+      ) : (
+        <div>
+          <div className="mainContainer">
+            {roomList.map((res) => (
+              <div className="user-profile-card">
+                <div className="avatar-section">
+                  {/* Avatar image would go here */}
+                  <div className="avatar-placeholder"></div>
+                </div>
+                <div className="user-info">
+                  <h2>{res.name}</h2>
+                  <p>
+                    {res.address.area}, {res.address.city}, {res.address.state}
+                  </p>
+                </div>
+                <div className="user-details">
+                  <div className="detail rent">Rent ₹ {res.amount}</div>
+                  <div className="detail looking-for">
+                    Looking for {res.requirement.gender}
+                  </div>
+                  <div className="detail flat-size">
+                    {res.requirement.size} Flat
+                  </div>
+                </div>
+                <p className="message">{res.requirement.message}</p>
               </div>
-              <div className="detail flat-size">
-                {res.requirement.size} Flat
-              </div>
-            </div>
-            <p className="message">{res.requirement.message}</p>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="buttonGroup">
-        <Button
-          variant="contained"
-          onClick={handlePrevButton}
-          disabled={isPrev}
-        >
-          Previous
-        </Button>
-        <Button variant="contained" onClick={handNextButton} disabled={isNext}>
-          Next
-        </Button>
-      </div>
+          <div className="buttonGroup">
+            <Button
+              variant="contained"
+              onClick={handlePrevButton}
+              disabled={isPrev}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="contained"
+              onClick={handNextButton}
+              disabled={isNext}
+            >
+              Next
+            </Button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
